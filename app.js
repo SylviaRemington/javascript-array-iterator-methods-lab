a113f59135c6a2c7572d17865dde6104fc736d40
SylviaRemington <sylviaremington@gmail.com>
2025-04-22 19:09:52 -0700

    beginning Exercise 5 - need to come back to this one

--- app.js
+++ app.js
@@ -275,3 +275,78 @@ console.log('Exercise 4 correct result: ', {
 
 //__________________________________________________________________________________________
 
+/*
+Exercise 5: Array.prototype.map()
+
+Use the Array.prototype.map() method to reformat each name in the 'people' 
+array. The goal is to convert names from "Last, First" format to "First Last" 
+format.
+
+Hint: Use the String.prototype.split() method to separate the first and last 
+      names. You can split the string using ', ' as the separator.
+      After splitting the names, rearrange them to the "First Last" format.
+
+PLAIN ENGLISH QUESTION:
+The question is asking you to: Look at each name in the people array.
+Each name is written like this: "Smith, John". You need to flip it to look like this instead: "John Smith".
+You should use the .map() method to go through the whole array, one name at a time.
+The hint is telling you:
+Use .split(', ') to break each name into two parts: last name and first name.
+Then rearrange those two parts into a new string with first name first, then last name.
+You're not changing the original array — you're creating a new one with the updated format.
+*/
+
+let firstLast = [];
+
+// Complete the exercise in the space below:
+
+firstLast = people.map (()) //JUST STARTED THIS PART... need to come back to it to figure it out.
+
+
+// Check your work:
+console.log('Exercise 5 my result: ', firstLast);
+console.log('Exercise 5 correct result: ', [
+  'Carl Becker',
+  'Samuel Beckett',
+  'Mick Beddoes',
+  'Henry Beecher',
+  'Ludwig Beethoven',
+  'Menachem Begin',
+  'Hilaire Belloc',
+  'Saul Bellow',
+  'Robert Benchley',
+  'Peter Benenson',
+  'David Ben-Gurion',
+  'Walter Benjamin',
+  'Tony Benn',
+  'Chester Bennington',
+  'Leana Benson',
+  'Silas Bent',
+  'Lloyd Bentsen',
+  'Ric Berger',
+  'Ingmar Bergman',
+  'Luciano Berio',
+  'Milton Berle',
+  'Irving Berlin',
+  'Eric Berne',
+  'Sandra Bernhard',
+  'Yogi Berra',
+  'Halle Berry',
+  'Wendell Berry',
+  'Erin Bethea',
+  'Aneurin Bevan',
+  'Ken Bevel',
+  'Joseph Biden',
+  'Ambrose Bierce',
+  'Steve Biko',
+  'Josh Billings',
+  'Frank Biondo',
+  'Augustine Birrell',
+  'Elk Black',
+  'Robert Blair',
+  'Tony Blair',
+  'William Blake',
+]);
+
+
+
