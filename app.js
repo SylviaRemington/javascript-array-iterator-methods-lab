6d0ad3af4b30747368bcec24264847a75ff18091
SylviaRemington <sylviaremington@gmail.com>
2025-04-22 20:34:15 -0700

    Level Up Reduce Method working on for level up 1 & 2

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 //Still need to complete Exercise 3
 //Still need to complete Exercise 5
+//Still need to complete Level Up Exercise 1 exercise
+//Still need to complete Level Up Exercise 2 exercise
+
 
 
 const inventors = [
@@ -488,3 +491,91 @@ console.log('Exercise 9 correct result: ', 3);
 
 
 //__________________________________________________________________________________________
+
+//LEVEL UP TIME!!!!
+
+/*
+Level Up exercise 1: Array.prototype.reduce()
+
+Calculate the combined lifespan of all the inventors using 
+Array.prototype.reduce()
+
+- Each object in the array includes these properties: 
+  'first', 'last', 'year' (birth year), and 'passed' (year of death).
+- Use the Array.prototype.reduce() method to calculate the sum of the total 
+  years lived by all the inventors.
+- Store the total sum in the variable 'totalYearsLived'.
+
+Hints:
+
+- Inside the reduce callback function, calculate the lifespan of each inventor 
+  (passed - year).
+- Accumulate this lifespan in the 'totalYearsLived' variable.
+- Remember, reduce takes a callback function and an initial value for the 
+  accumulator.
+*/
+
+let totalYearsLived = 0;
+
+// Complete the exercise in the space below:
+
+totalYearsLived = inventors.reduce((accumulatedValueHere?, inventor))//need to research this reduce method more
+
+// Check your work:
+console.log('Level Up 1 my result: ', totalYearsLived);
+console.log('Level Up 1 correct result: ', 861);
+
+//__________________________________________________________________________________________
+
+// const travelMethods = [
+//     'car',
+//     'car',
+//     'truck',
+//     'truck',
+//     'bike',
+//     'walk',
+//     'car',
+//     'van',
+//     'bike',
+//     'walk',
+//     'car',
+//     'van',
+//     'car',
+//     'truck',
+//   ];
+  
+/*
+Level Up exercise 2: Array.prototype.reduce()
+
+Tallying travel methods using Array.prototype.reduce(). 
+
+Count the number of times each travel method appears in the 'travelMethods'
+array.
+
+- The resulting object should have keys as the travel methods 
+  ('car', 'truck', 'bike', etc.) and values as their respective counts.
+- Store this object in the variable 'travelMethodCounts'.
+
+Hints:
+- Inside the reduce function, check if the travel method already exists as a key
+  in your accumulator object. If it does, increment its count. If not, add it 
+  to the object and give it a value of 1.
+- Since you want to return an object, be sure to pass an empty {} for the 
+  initial value of the "accumulator".
+*/
+
+let travelMethodCounts = {};
+
+// Complete the exercise in the space below:
+
+travelMethodCounts = travelMethods.reduce(())//need to research this reduce method more
+
+// Check your work:
+console.log('Level Up 2 my result: ', travelMethodCounts);
+console.log('Level Up 2 correct result: ', {
+  car: 5,
+  truck: 3,
+  bike: 2,
+  walk: 2,
+  van: 2,
+});
